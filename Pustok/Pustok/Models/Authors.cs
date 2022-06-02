211b1e9c31e0aabfbcbd77b11c8ebc591268ad9b
Resul023 <73m6u7h@code.edu.az>
2022-06-02 01:52:03 -0700

    HomeWork

---
+++ Pustok/Pustok/Models/Authors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Models
+{
+    public class Authors
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(maximumLength:100)]
+        public string FullName { get; set; }
+    }
+}
---
+++ Pustok/Pustok/Models/Genres.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Models
+{
+    public class Genres
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(maximumLength:60)]
+        public string Name { get; set; }
+    }
+}
