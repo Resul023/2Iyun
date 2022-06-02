204849050ee295c3fe1bc8060bc8efdeed618b1d
Resul023 <73m6u7h@code.edu.az>
2022-06-02 02:27:59 -0700

    HomeWork

---
+++ Pustok/Pustok/Controllers/ShopController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pustok.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Pustok.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pustok.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ShopController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            ShopViewModel shopVW = new ShopViewModel
+            {
+                Genre = _context.Genres.ToList(),
+                book = _context.Book.ToList(),
+               
+            };
+
+            return View(shopVW);
+        }
+    }
+}
